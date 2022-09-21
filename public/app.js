fbb03c296e170470343c5fc526a45e01ff7edf5e
axelhagerback <axel.hagerback@gmail.com>
2022-09-21 14:40:21 +0200

    primenumber list exercise done

--- public/app.js
+++ public/app.js
@@ -1,11 +1,19 @@
 console.log('Hello world');
 
 function primeList() {
+    writePrimeList.innerHTML = '';
     const inputListNum = document.getElementById('inputPrimeList').value;
-    console.log(inputListNum);
+
+    const primeNumberArray = [2, 3, 5, 7, 11];
+
+    for(var i=0; i < inputListNum; i++) {
+        writePrimeList.innerHTML += primeNumberArray[i] + ', ';
+    }
 };
 
 function primePosition() {
     const inputPositionNum = document.getElementById('inputPrimePosition').value;
-    console.log(inputPositionNum);
+
+
+    
 }
